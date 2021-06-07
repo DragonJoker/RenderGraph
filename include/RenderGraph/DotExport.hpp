aa443477efbed52308d8cc9556238e38e266238d
DragonJoker <dragonjoker59@hotmail.com>
2021-06-07 23:50:43 +0200

    Dot export colours for external source/destination.

--- include/RenderGraph/DotExport.hpp
+++ include/RenderGraph/DotExport.hpp
@@ -9,10 +9,6 @@ See LICENSE file in root folder.
 
 namespace crg::dot
 {
-	static std::string_view constexpr imgColour{ "#8b008b" };
-	static std::string_view constexpr bufColour{ "#458b00" };
-	static std::string_view constexpr passColour{ "#00007f" };
-
 	CRG_API void displayPasses( std::ostream & stream
 		, RunnableGraph const & value
 		, bool withColours );
