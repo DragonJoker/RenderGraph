ce73fa51c9e2f4e16fb094b72f05423cf0580367
Sylvain Doremus <dragonjoker59@hotmail.com>
2024-03-01 18:17:57 +0100

    Some fixes for AppleClang and MacOS.

--- source/RenderGraph/GraphContext.cpp
+++ source/RenderGraph/GraphContext.cpp
@@ -144,7 +144,16 @@ namespace crg
 
 	GraphContext::~GraphContext()noexcept
 	{
-		doReportRegisteredObjects();
+#if VK_EXT_debug_utils || VK_EXT_debug_marker
+		for ( auto const & [_, alloc] : m_allocated )
+		{
+			fprintf( stderr
+				, "Leaked [%.20s](%.90s), allocation stack:\n%.900s"
+				, alloc.type.c_str()
+				, alloc.name.c_str()
+				, alloc.callstack.c_str() );
+		}
+#endif
 	}
 
 #if VK_EXT_debug_utils || VK_EXT_debug_marker
@@ -375,19 +384,6 @@ namespace crg
 					<< " - Unregistered";
 				Logger::logTrace( stream.str() );
 			}
-		}
-	}
-
-	void GraphContext::doReportRegisteredObjects()noexcept
-	{
-		lock_type lock{ m_mutex };
-
-		for ( auto const & [_, alloc] : m_allocated )
-		{
-			std::stringstream stream;
-			stream << "Leaked [" << alloc.type << "](" << alloc.name << "), allocation stack:\n";
-			stream << alloc.callstack;
-			Logger::logError( stream.str() );
 		}
 	}
 
